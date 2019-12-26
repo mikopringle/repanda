fb2d472f27f68ed09654bdbb1dd21d4c1e01b5a6
Frank <b439988l@gmail.com>
2019-12-26 21:13:37 +1000

    states for input boxes

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/register.css'
 
 export function Register(props) {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [name, setName] = useState("")
+
     return(
         <form className="register">
             <label>
                 Email:
-                <input></input>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}></input>
                 Password:
-                <input></input>
+                <input value={password} onChange={(e) => setPassword(e.target.value)}></input>
                 Company Name:
-                <input></input>
+                <input value={name} onChange={(e) => setName(e.target.value)}></input>
                 <input type="submit" value="Submit" />
             </label>
         </form>
