95d78b034fccb1826236f4b4edc17d28de0003f2
Frank <b439988l@gmail.com>
2019-12-30 10:18:42 +1000

    more input fields for sign up page

--- src/pages/register.js
+++ src/pages/register.js
@@ -5,6 +5,13 @@ export function Register(props) {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [name, setName] = useState("")
+    const [inudstry, setIndustry] = useState("")
+    const [code, setCode] = useState("")
+    const [abn, setAbn] = useState("")
+
+    const submit = () => {
+
+    }
 
     return(
         <form className="register">
@@ -15,7 +22,13 @@ export function Register(props) {
                 <input value={password} onChange={(e) => setPassword(e.target.value)}></input>
                 Company Name:
                 <input value={name} onChange={(e) => setName(e.target.value)}></input>
-                <input type="submit" value="Submit" />
+                Company Industry:
+                <input value={inudstry} onChange={(e) => setIndustry(e.target.value)}></input>
+                Company ABN:
+                <input value={abn} onChange={(e) => setAbn(e.target.value)} />
+                Invitation Code:
+                <input value={code} onChange={(e) => setCode(e.target.value)} />
+                <input type="submit" value="Submit" onClick={submit}/>
             </label>
         </form>
     );
