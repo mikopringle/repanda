45f5dcab623cdb1ef04d4afc2ac29310b8402ad1
Frank <b439988l@gmail.com>
2020-01-04 13:22:09 +1000

    started working on dash

--- src/pages/dash.js
+++ src/pages/dash.js
@@ -1,5 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { get } from '../api/api'
 
 export function Dash(props){
+    const [measures, setMeasures] = useState([])
+    const [averages, setAverages] = useState([])
+
+    useEffect(() => {
+        
+    },[])
     return <h1>Big Dash</h1>
 }
