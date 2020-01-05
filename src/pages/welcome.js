135d1342cec4752d301842244dd3680a740d528a
Frank <b439988l@gmail.com>
2020-01-05 13:30:05 +1000

    componentalised linkedBtn

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -1,23 +1,17 @@
 import React from 'react'
-import { Trisection } from '../components/trisection'
-import { Link } from 'react-router-dom'
-
-
-const top = <h1>SurveyPanda</h1>
-
-const mid = <div>
-    <p>Employees are your most important asset</p>
-    <p>Keeping them engaged make them more productive and reduces attrition, saving you time and money usually spend on hiring</p>
-    <p>Let's get your free trial started</p>
-</div>
-
-const bottom = <div>
-    <Link to="/register">Register</Link>
-    <Link to="/login">Login</Link>
-</div>
+import Button from '../components/linkedBtn'
 
 export function Welcome(props) {
     return (
-        <Trisection top={top} mid={mid} bottom={bottom} />
+        <div>
+            <h1>SurveyPanda</h1>
+            <div className="text-left">
+                <p >Employees are your most important asset</p>
+                <p>Keeping them engaged make them more productive and reduces attrition, saving you time and money usually spend on hiring</p>
+                <p>Let's get your free trial started</p>
+            </div>
+            <Button link="/register" text="Register" />
+            <Button link="/login" text="Log In" />
+        </div>
     );
 }
