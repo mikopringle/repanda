273551fcd1a2be9fb7fc01d2359cc4d010d6585e
Miko Pringle <mikopringle@outlook.com>
2020-01-06 16:27:42 +1000

    Added styling to the welcome page

--- src/components/linkedBtn.js
+++ src/components/linkedBtn.js
@@ -3,6 +3,6 @@ import { Link } from 'react-router-dom'
 
 export default function Button ({link, text}){
     return (
-    <button className="font-bold py-2 px-4 rounded inline-block"><Link to={link}>{text}</Link></button>
+    <button className="font-bold mt-8 py-2 px-3 text-xl rounded-lg inline-block bg-teal-500 text-white"><Link to={link}>{text}</Link></button>
     )
 }
--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -1,41 +1,23 @@
 import React from 'react'
-<<<<<<< HEAD
-import { Trisection } from '../components/trisection'
-import { Link } from 'react-router-dom'
-import '../css/welcome.css'
-
-
-const top = <h1>SurveyPanda</h1>
-
-const mid = <div>
-    <p>Employees are your most important asset</p>
-    <p>Keeping them engaged make them more productive and reduces attrition, saving you time and money usually spend on hiring</p>
-    <p>Let's get your free trial started</p>
-</div>
-
-const bottom = <div>
-    <Link to="/register">Register</Link>
-    <Link to="/login">Login</Link>
-</div>
-
-export function Welcome(props) {
-    return (
-        <Trisection top={top} mid={mid} bottom={bottom} />
-=======
 import Button from '../components/linkedBtn'
 
 export function Welcome(props) {
     return (
         <div>
-            <h1>SurveyPanda</h1>
+            <h1  className= "bg-teal-500 text-white text-2xl font-bold font-sans flex items-center justify-center" >SurveyPanda</h1>
             <div className="text-left">
                 <p >Employees are your most important asset</p>
                 <p>Keeping them engaged make them more productive and reduces attrition, saving you time and money usually spend on hiring</p>
                 <p>Let's get your free trial started</p>
             </div>
-            <Button link="/register" text="Register" />
+            <div className = "flex items-center justify-center ">
+            <div className="mr-8">
+            <Button  link="/register" text="Register" />
+            </div>
+            <div className="ml-8">
             <Button link="/login" text="Log In" />
+            </div>
+            </div>
         </div>
->>>>>>> 135d1342cec4752d301842244dd3680a740d528a
     );
 }
