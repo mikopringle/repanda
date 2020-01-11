a5d4b1f8128b6c07de3fc29f25c326e73face242
Frank <b439988l@gmail.com>
2020-01-11 22:26:30 +1000

    questionnaire page api call - error with backend

--- src/pages/question.js
+++ src/pages/question.js
@@ -1,5 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { get } from '../api/api'
 
 export function Questionnaire(props){
-    return <div></div>
+    const [questions, setQuestions] = useState([])
+    const [answers, setAnswers] = useState([])
+
+    useEffect(() => {
+        const apiCall = async (route) => {
+            try {
+                const res = await get(route)
+                console.log(res)
+            }
+            catch (err) {
+                console.log(err.message)
+            }
+        }
+        apiCall(`question/today?id=${props.compayId}`)
+    },[])
+
+    return <div>{}</div>
 } 
