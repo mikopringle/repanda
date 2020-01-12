8c7320f9ba4bf6bb2991fedc8f1c1066887f889e
Miko Pringle <mikopringle@outlook.com>
2020-01-12 12:34:36 +1000

    Added more styling to Login and Register

--- src/pages/register.js
+++ src/pages/register.js
@@ -51,32 +51,47 @@ export function Register(props) {
 
     const form = <div className="register">
         <label>
-            <h1 className="bg-teal-500 text-white text-2xl font-bold font-sans flex items-center justify-center" >SurveyPanda</h1>
-            <div className="text-gray-700 text-xl font-bold mb-1">
+            <h1 className="bg-teal-500 text-white text-4xl font-bold font-sans flex items-center justify-center" >SurveyPanda</h1>
+
+            <div className="text-gray-700 text-xl font-bold mb-1 mt-5 flex items-center justify-center">
                 Company Name:
            </div>
-            <input className="appearance-none block  bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-6 leading-tight focus:outline-none focus:bg-white w-64" value={name} onChange={(e) => setName(e.target.value)}></input>
-            <div className="text-gray-700 text-xl font-bold mb-1">
+            <div className=" flex items-center justify-center">
+                <input className="appearance-none block  bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-6 leading-tight focus:outline-none focus:bg-white w-64" value={name} onChange={(e) => setName(e.target.value)}></input>
+            </div>
+            <div className="text-gray-700 text-xl font-bold mb-1 flex items-center justify-center">
                 Company Industry:
            </div>
-            <input className="appearance-none block bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-6 leading-tight focus:outline-none focus:bg-white w-64" value={inudstry} onChange={(e) => setIndustry(e.target.value)}></input>
-            <div className="text-gray-700 text-xl font-bold mb-1">
+            <div className=" flex items-center justify-center flex items-center justify-center">
+                <input className="appearance-none block bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-6 leading-tight focus:outline-none focus:bg-white w-64" value={inudstry} onChange={(e) => setIndustry(e.target.value)}></input>
+            </div>
+            <div className="text-gray-700 text-xl font-bold mb-1 flex items-center justify-center">
                 Email:
            </div>
-            <input className="appearance-none block bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-6 leading-tight focus:outline-none focus:bg-white w-64" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-            <div className="text-gray-700 text-xl font-bold mb-1">
+            <div className=" flex items-center justify-center">
+                <input className="appearance-none block bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-6 leading-tight focus:outline-none focus:bg-white w-64" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+            </div>
+            <div className="text-gray-700 text-xl font-bold mb-1 flex items-center justify-center">
                 Password:
            </div>
-            <input className="appearance-none block  bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-6 leading-tight focus:outline-none focus:bg-white w-64" value={password} type="password" onChange={(e) => setPassword(e.target.value)}></input>
-            <div className="text-gray-700 text-xl font-bold mb-1">
+            <div className="flex items-center justify-center">
+                <input className="appearance-none block  bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-6 leading-tight focus:outline-none focus:bg-white w-64" value={password} type="password" onChange={(e) => setPassword(e.target.value)}></input>
+            </div>
+            <div className="text-gray-700 text-xl font-bold mb-1 flex items-center justify-center">
                 Company ABN:
            </div>
-            <input className="appearance-none block  bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-6 leading-tight focus:outline-none focus:bg-white w-64" value={abn} onChange={(e) => setAbn(e.target.value)} />
-            <div className="text-gray-700 text-xl font-bold mb-1">
+            <div className="flex items-center justify-center">
+                <input className="appearance-none block  bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-6 leading-tight focus:outline-none focus:bg-white w-64" value={abn} onChange={(e) => setAbn(e.target.value)} />
+            </div>
+            <div className="text-gray-700 text-xl font-bold mb-1 flex items-center justify-center">
                 Invitation Code:
            </div>
-            <input className="appearance-none block  bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-6 leading-tight focus:outline-none focus:bg-white w-64" value={code} onChange={(e) => setCode(e.target.value)} />
+            <div className="flex items-center justify-center">
+                <input className="appearance-none  bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-6 leading-tight focus:outline-none focus:bg-white w-64" value={code} onChange={(e) => setCode(e.target.value)} />
+            </div>
+            <div className = "flex items-center justify-center">
             <button className="font-bold  py-2 px-3 text-xl rounded-lg bg-teal-500 text-white flex items-center justify-center" onClick={submit}>Submit</button>
+            </div>
         </label>
     </div>
 
