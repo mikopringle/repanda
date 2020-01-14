887256127b6aaa37268d742772b33788d04404ef
Frank <b439988l@gmail.com>
2020-01-14 14:47:42 +1000

    left aligned labels

--- src/components/formElements.js
+++ src/components/formElements.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export const StyledLabel = (props) => <div className="text-gray-700 text-xl font-bold mb-1">{props.text}</div>
+export const StyledLabel = (props) => <div className="text-gray-700 font-bold mb-1 text-left">{props.text}</div>
 
 export const StyledInput = ({value, setValue, type, placeholder}) => <div className="block">
         <input 
-            className="appearance-none text-gray-700 border rounded py-3 px-4 mb-6 leading-tight focus:outline-none focus:border-teal-500" 
+            className="appearance-none w-full text-gray-700 border rounded py-3 px-4 mb-6 leading-tight focus:outline-none focus:border-teal-500" 
             value={value} 
             onChange={(e) => setValue(e.target.value)} 
             placeholder={placeholder}
